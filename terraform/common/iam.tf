2d57d0953f1a3b3ec375e23ee5356b48669ba8d4
William Fish <william.michael.fish@gmail.com>
2022-06-20 14:44:41 +0100

    HOTT-1691: Fix broken ECR iam configuration

--- terraform/common/iam.tf
+++ terraform/common/iam.tf
@@ -39,54 +39,57 @@ EOF
 
 resource "aws_iam_policy" "ecr_policy" {
   name        = "ecr-policy"
-  description = "A test policy"
+  description = "ECR policy for managing project deployment ECR images"
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": [
-                "ecr:PutLifecyclePolicy",
-                "ecr:PutImageTagMutability",
-                "ecr:DescribeImageScanFindings",
-                "ecr:StartImageScan",
-                "ecr:GetLifecyclePolicyPreview",
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:PutImageScanningConfiguration",
-                "ecr:ListTagsForResource",
-                "ecr:InitiateLayerUpload",
-                "ecr:UploadLayerPart",
-                "ecr:ListImages",
-                "ecr:PutImage",
-                "ecr:BatchGetImage",
-                "ecr:CompleteLayerUpload",
-                "ecr:DescribeImages",
-                "ecr:TagResource",
-                "ecr:DescribeRepositories",
-                "ecr:StartLifecyclePolicyPreview",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:GetRepositoryPolicy",
-                "ecr:GetLifecyclePolicy"
-            ],
-            "Resource": "arn:aws:ecr:eu-west-2:777015734912:repository/*"
-        },
-        {
-            "Sid": "VisualEditor1",
-            "Effect": "Allow",
-            "Action": [
-                "ecr:GetRegistryPolicy",
-                "ecr:DescribeRegistry",
-                "ecr:GetAuthorizationToken",
-                "ecr:PutRegistryPolicy"
-            ],
-            "Resource": "*"
-        }
-    ]
+  policy = data.aws_iam_policy_document.ecr.json
 }
-EOF
+
+data "aws_iam_policy_document" "ecr" {
+  statement {
+    sid = "VisualEditor0"
+
+    actions = [
+      "ecr:PutLifecyclePolicy",
+      "ecr:PutImageTagMutability",
+      "ecr:DescribeImageScanFindings",
+      "ecr:StartImageScan",
+      "ecr:GetLifecyclePolicyPreview",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:PutImageScanningConfiguration",
+      "ecr:ListTagsForResource",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:ListImages",
+      "ecr:PutImage",
+      "ecr:BatchGetImage",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeImages",
+      "ecr:TagResource",
+      "ecr:DescribeRepositories",
+      "ecr:StartLifecyclePolicyPreview",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetRepositoryPolicy",
+      "ecr:GetLifecyclePolicy"
+    ]
+
+    resources = [
+      "arn:aws:ecr:eu-west-2:777015734912:repository/*"
+    ]
+  }
+
+  statement {
+    sid = "VisualEditor1"
+    actions = [
+      "ecr:GetRegistryPolicy",
+      "ecr:DescribeRegistry",
+      "ecr:GetAuthorizationToken",
+      "ecr:PutRegistryPolicy"
+    ]
+
+    resources = [
+      "arn:aws:ecr:eu-west-2:777015734912:repository/*"
+    ]
+  }
 }
 
 resource "aws_iam_user_policy_attachment" "ci-ecr" {
